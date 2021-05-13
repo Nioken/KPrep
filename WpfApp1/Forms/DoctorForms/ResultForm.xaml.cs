0c09b0ac23088abcf94f26baa1355a5aa712b754
Nioken <Nioken85@gmail.com>
2021-05-13 18:47:03 +0300

    FormatBarAdded

--- WpfApp1/Forms/DoctorForms/ResultForm.xaml.cs
+++ WpfApp1/Forms/DoctorForms/ResultForm.xaml.cs
@@ -29,25 +29,39 @@ namespace WpfApp1.Forms.DoctorForms
             this.SelectedTicket = SelectedTicket;
             InfoLabel.Content = SelectedTicket.PaidServices.Name + ": " + SelectedTicket.Client.Surname + " " + SelectedTicket.Client.Name + " " + SelectedTicket.Client.Lastname;
         }
-        public static string GetText(RichTextBox rtb)
-        {
-            TextRange textRange = new TextRange(rtb.Document.ContentStart, rtb.Document.ContentEnd);
-            string text = textRange.Text;
-            return text;
-        }
+        //public static string GetText(RichTextBox rtb)
+        //{
+        //    TextRange textRange = new TextRange(rtb.Document.ContentStart, rtb.Document.ContentEnd);
+        //    string text = textRange.Text;
+        //    return text;
+        //}
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (!String.IsNullOrWhiteSpace(GetText(ResultBox)))
+            //if (!String.IsNullOrWhiteSpace(GetText(ResultBox.Text)))
+            if (!String.IsNullOrWhiteSpace(ResultBox.Text))
             {
                 Result res = new Result();
                 res.ResultID = MyDBContext.DBContext.Results.Count();
                 res.ResultID++;
 
-                res.ResultText = GetText(ResultBox);
+                res.ResultText = ResultBox.Text;//res.ResultText = GetText(ResultBox);
                 res.Ticket = SelectedTicket;
                 MyDBContext.DBContext.Results.Add(res);
                 MyDBContext.DBContext.SaveChanges();
                 this.Close();
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var Results = MyDBContext.DBContext.Results.ToList();
+            for(int i = 0; i < Results.Count(); i++)
+            {
+                if(Results[i].Ticket == SelectedTicket)
+                {
+                    ResultBox.Text = Results[i].ResultText;
+                    break;
+                }
             }
         }
     }
